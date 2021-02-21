31df55ba5d7f40931e0bdf57c0c685bac1548b2a
遠藤達也 <kmno4hno3@ezweb.ne.jp>
2021-02-21 13:07:18 +0900

    twitterのOAuth認証の登録できた

--- api/app/controllers/api/v1/omniauth_callbacks_controller.rb
+++ api/app/controllers/api/v1/omniauth_callbacks_controller.rb
@@ -24,11 +24,11 @@ class Api::V1::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksC
   protected
   def get_redirect_route(devise_mapping)
     p "get_redirect_route"
-    path = "#{Devise.mappings[devise_mapping.to_sym].fullpath}#{params[:provider]}/callback"
-    p path
-      klass = request.scheme == 'https' ? URI::HTTPS : URI::HTTP
-      redirect_route = klass.build(host: request.host, port: request.port, path: path).to_s
-    # super
+    # path = "#{Devise.mappings[devise_mapping.to_sym].fullpath}#{params[:provider]}/callback"
+    # p path
+    #   klass = request.scheme == 'https' ? URI::HTTPS : URI::HTTP
+    #   redirect_route = klass.build(host: request.host, port: request.port, path: path).to_s
+    super
   end
 
   def assign_provider_attrs(user, auth_hash)
